494f7e3af8cf9f8db7979bb79de1c5c1a3d9c81f
hboussard <hugues.boussard@inra.fr>
2017-05-05 14:52:17 +0200

    init src

---
+++ src/fr/inra/sad/bagap/chloe4qgis/Main.java
@@ -0,0 +1,127 @@
+package fr.inra.sad.bagap.chloe4qgis;
+
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		treatParamsAndRun(args);
+	}
+	
+	private static void treatParamsAndRun(String[] args){
+		
+		int index;
+		
+		String[] inputMatrix, metrics, windowSizes, filters, unfilters;
+		
+		String processType, csvOutput, shape, friction, frictionMatrix, delta, interpolate, minRate, exportCsv, exportAscii, outputFolder;
+		
+		switch(args[1]){
+		case "map" : 
+			// ex : chloe map {c://hugues/temp/chloe/carte.asc} qualitative {SHDI} c://hugues/temp/chloe/maptest.csv
+					
+			index = 2;
+			
+			// récupération des cartes
+			inputMatrix = getSplitString(args[index++]);
+				
+			// récupération du type de process
+			processType = getString(args[index++]);
+					
+			// récupération des métriques
+			metrics = getSplitString(args[index++]);
+					
+			// récupération du csv output
+			csvOutput = getString(args[index++]);
+			
+			// lancement du traitement apiland
+			Model.runMapWindow(inputMatrix, processType, metrics, csvOutput);
+					
+			break;
+			
+		case "sliding" :
+			// ex : chloe sliding {c://hugues/temp/chloe/carte.asc} qualitative circle null null {21} 1 false 100 {SHDI} c://hugues/temp/chloe/output/ true true null null
+			// inputMatrix processType shape friction frictionMatrix windowSizes delta interpolate minRate metrics outputFolder exportCsv exportAscii filters unfilters
+			
+			index = 2;
+			
+			// récupération des cartes
+			inputMatrix = getSplitString(args[index++]);
+							
+			// récupération du type de process
+			processType = getString(args[index++]);
+			
+			// récupération de la forme de fenêtre
+			shape = getString(args[index++]);
+			
+			// récupération du fichier de friction
+			friction = getString(args[index++]);
+			
+			// récupération de la carte de friction
+			frictionMatrix = getString(args[index++]);
+			
+			// récupération des tailles de fenêtres
+			windowSizes = getSplitString(args[index++]);
+			
+			// récupération du delta
+			delta = getString(args[index++]);
+			
+			// récupération de l'interpolation
+			interpolate = getString(args[index++]);
+			
+			// récupération du minRate
+			minRate = getString(args[index++]);
+			
+			// récupération des métriques
+			metrics = getSplitString(args[index++]);
+			
+			// récupération du dossier d'output
+			outputFolder = getString(args[index++]);
+								
+			// récupération de l'export en csv
+			exportCsv = getString(args[index++]);
+			
+			// récupération de l'export en ascii
+			exportAscii = getString(args[index++]);
+			
+			// récupération des filtres
+			filters = getSplitString(args[index++]);
+						
+			// récupération des unfiltres
+			unfilters = getSplitString(args[index++]);
+			
+			// lancement du traitement apiland
+			Model.runSlidingWindow(inputMatrix, processType, shape, friction, frictionMatrix, windowSizes, delta, interpolate, minRate, metrics, outputFolder, 
+					exportCsv, exportAscii, filters, unfilters);
+			
+			break;
+			
+		default : throw new IllegalArgumentException (args[1]+" n'est pas reconnu comme commande pour Chloe");	
+		}
+	}
+
+	
+	private static String getString(String param){
+		if(!param.equalsIgnoreCase("null")){
+			return param;
+		}
+		return "";
+	}
+	
+	/**
+	 * recuperation des String multiples
+	 */
+	private static String[] getSplitString(String param){
+		if(!param.equalsIgnoreCase("null")){
+			StringTokenizer st = new StringTokenizer(param.replace(" ", "").replace("{", "").replaceAll("}", ""));
+			String[] tab = new String[st.countTokens()];
+			int index = 0; 
+			while(st.hasMoreTokens()){
+				tab[index++] = st.nextToken();
+			}
+			return tab;
+		}
+		return null;
+	}
+	
+}
---
+++ src/fr/inra/sad/bagap/chloe4qgis/Model.java
@@ -0,0 +1,200 @@
+package fr.inra.sad.bagap.chloe4qgis;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import com.csvreader.CsvWriter;
+import fr.inra.sad_paysage.apiland.core.space.impl.raster.matrix.Friction;
+import fr.inra.sad_paysage.apiland.core.space.impl.raster.matrix.Matrix;
+import fr.inra.sad_paysage.apiland.treatment.Treatment;
+import fr.inra.sad_paysage.apiland.treatment.TreatmentException;
+import fr.inra.sad_paysage.apiland.treatment.window.MapWindowMatrixTreatment;
+import fr.inra.sad_paysage.apiland.treatment.window.SlidingWindowMatrixTreatment;
+import fr.inra.sad_paysage.apiland.window.shape.WindowShapeType;
+
+public class Model {
+
+
+	public static boolean runMapWindow(String[] inputMatrix, String processType, String[] metrics, String csvOutput) {
+		
+		Treatment treatment = new MapWindowMatrixTreatment();
+		
+		Set<String> myMetrics = Util.getSetString(metrics);
+		
+		try{
+			
+			// settings
+			if(processType.equalsIgnoreCase("qualitative")){
+				treatment.setInput("qualitative", true);
+			}else{
+				treatment.setInput("qualitative", false);
+			}
+			treatment.setInput("metrics", myMetrics);
+			
+			try {
+				CsvWriter cw = new CsvWriter(csvOutput);
+				cw.setDelimiter(';');
+				
+				cw.write("name");
+				for(String m : myMetrics){
+					cw.write(m);
+				}
+				
+				cw.endRecord();
+				
+				for(String matrix : inputMatrix){
+					//publish("treatment of the matrix "+matrix);
+					//treatment.addView(this);
+					//progress = 0;
+					
+					treatment.setInput("matrix", Util.getMatrix(matrix));
+					
+					treatment.setInput("csv", cw);
+					
+					// treatment
+					try{
+						treatment.allRun();
+						
+						treatment.clearViews();
+						treatment.clearOutputs();
+					} catch(TreatmentException e){
+						e.printStackTrace();
+					}
+				}
+				
+				cw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+			
+			return true;
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static boolean runSlidingWindow(String[] inputMatrix, String processType, String shape, String friction, String frictionMatrix,
+			String[] windowSizes, String delta, String interpolate, String minRate, String[] metrics,
+			String outputFolder, String exportCsv, String exportAscii,
+			String[] filters, String[] unfilters) {
+		
+		/*
+		 * Set<Matrix> inputMatrix,
+			String processType, WindowShapeType shape, Friction friction, Matrix frictionMatrix,
+			List<Integer> windowSizes, int delta, boolean interpolate, double minRate, Set<String> metrics,
+			String outputFolder, boolean viewAsciiOutput, boolean exportCsv, boolean exportAscii,
+			Set<Integer> filters, Set<Integer> unfilters
+		 */
+		
+		Treatment treatment = new SlidingWindowMatrixTreatment();
+		
+		Set<String> myMetrics = Util.getSetString(metrics);
+		WindowShapeType myShape = Util.getShape(shape);
+		int myDelta = Util.getInteger(delta);
+		boolean myInterpolate = Util.getBoolean(interpolate);
+		double myMinRate = Util.getDouble(minRate);
+		Set<Integer> myFilters = Util.getSetInteger(filters);
+		Set<Integer> myUnfilters = Util.getSetInteger(filters);
+		Friction myFriction = Util.getFriction(friction);
+		Matrix myFrictionMatrix = Util.getMatrix(frictionMatrix);
+		List<Integer> myWindowSizes = Util.getListInteger(windowSizes);
+		boolean myExportCsv = Util.getBoolean(exportCsv);
+		boolean myExportAscii = Util.getBoolean(exportAscii);
+		
+		try{
+			// settings
+			if(processType.equalsIgnoreCase("qualitative")){
+				treatment.setInput("qualitative", true);
+			}else{
+				treatment.setInput("qualitative", false);
+			}
+			treatment.setInput("shape", myShape);
+			treatment.setInput("delta", myDelta);
+			treatment.setInput("interpolation", myInterpolate);
+			treatment.setInput("min_rate", myMinRate);
+			treatment.setInput("metrics", myMetrics);
+			treatment.setInput("filters", myFilters);
+			treatment.setInput("unfilters", myUnfilters);
+			if(myFriction != null){
+				treatment.setInput("friction_map", myFriction);
+			}
+			if(myFrictionMatrix != null){
+				treatment.setInput("friction_matrix", myFrictionMatrix);
+			}
+			
+			if(myShape == WindowShapeType.FUNCTIONAL){
+				for(String matrix : inputMatrix){
+					String name = new File(matrix).getName().replace(".asc", "");
+					
+					treatment.setInput("matrix", Util.getMatrix(matrix));
+					
+					for(int wsize : myWindowSizes){
+			
+						//publish("treatment of the matrix "+name+" using window size = "+wsize);
+						//System.out.println("treatment of the matrix "+name+" using window size = "+wsize);
+						//treatment.addView(this);
+						//progress = 0;
+						
+						List<Integer> ws = new ArrayList<Integer>();
+						ws.add(wsize);
+						treatment.setInput("window_sizes", ws);
+						if(myExportCsv){
+							treatment.setInput("csv", outputFolder+"/"+name+"_"+WindowShapeType.getAbreviation(myShape)+"_w"+wsize+".csv");
+						}
+						if(myExportAscii){
+							treatment.setInput("ascii", outputFolder+"/"+name+"_"+WindowShapeType.getAbreviation(myShape)+"_w"+wsize+"_");
+						}
+						
+						// treatment
+						try{
+							treatment.allRun();
+							
+							treatment.clearViews();
+							treatment.clearOutputs();
+						} catch(TreatmentException e){
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			else{
+				for(String matrix : inputMatrix){
+					String name = new File(matrix).getName().replace(".asc", "");
+					
+					treatment.setInput("matrix", Util.getMatrix(matrix));
+					
+					//publish("treatment of the matrix "+name+" using window size = "+windowSizes);
+					//treatment.addView(this);
+					//progress = 0;
+					
+					treatment.setInput("window_sizes", myWindowSizes);
+					if(myExportCsv){
+						treatment.setInput("csv", outputFolder+"/"+name+"_"+WindowShapeType.getAbreviation(myShape)+".csv");
+					}
+					if(myExportAscii){
+						treatment.setInput("ascii", outputFolder+"/"+name+"_"+WindowShapeType.getAbreviation(myShape)+"_");
+					}
+					
+					// treatment
+					try{
+						treatment.allRun();
+						
+						treatment.clearViews();
+						treatment.clearOutputs();
+					} catch(TreatmentException e){
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			return true;
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+}
